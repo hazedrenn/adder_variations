9cad5df2dd177c21114cf132ca74785af70bca5d
Francisco <franciscolhernan@gmail.com>
2025-09-25 14:22:20 -0700

    set tabs to spaces

--- sim/half_adder_tb.vhd
+++ sim/half_adder_tb.vhd
@@ -11,59 +11,59 @@ entity half_adder_tb is
 end entity half_adder_tb;
 
 architecture behavior of half_adder_tb is
-	component half_adder is
-	port(
-		x: in std_logic;
-		y: in std_logic;
-		cout: out std_logic;
-		sum: out std_logic);
-	end component half_adder;
+  component half_adder is
+  port(
+    x: in std_logic;
+    y: in std_logic;
+    cout: out std_logic;
+    sum: out std_logic);
+  end component half_adder;
 
-	procedure adder_result(
-		x: std_logic;
-		y: std_logic;
-		cout: std_logic; 
-		sum: std_logic) is
-		variable x_str : string(0 to 0) := to_string(x);
-		variable y_str : string(0 to 0) := to_string(y);
-		variable cout_str : string(0 to 0) := to_string(cout);
-		variable sum_str : string(0 to 0) := to_string(sum);
-	begin
-		print(x_str&" + "&y_str&" = "&cout_str&sum_str);
-	end procedure adder_result;
-	
-	signal signal_x: std_logic;
-	signal signal_y: std_logic;
-	signal signal_cout: std_logic;
-	signal signal_sum: std_logic;
-	constant PERIOD: time := 1 ns;
+  procedure adder_result(
+    x: std_logic;
+    y: std_logic;
+    cout: std_logic; 
+    sum: std_logic) is
+    variable x_str : string(0 to 0) := to_string(x);
+    variable y_str : string(0 to 0) := to_string(y);
+    variable cout_str : string(0 to 0) := to_string(cout);
+    variable sum_str : string(0 to 0) := to_string(sum);
+  begin
+    print(x_str&" + "&y_str&" = "&cout_str&sum_str);
+  end procedure adder_result;
+  
+  signal signal_x: std_logic;
+  signal signal_y: std_logic;
+  signal signal_cout: std_logic;
+  signal signal_sum: std_logic;
+  constant PERIOD: time := 1 ns;
 begin
-	half_adder1: component half_adder
-	port map (
-		x => signal_x,
-		y => signal_y,
-		cout => signal_cout,
-		sum => signal_sum);
+  half_adder1: component half_adder
+  port map (
+    x => signal_x,
+    y => signal_y,
+    cout => signal_cout,
+    sum => signal_sum);
 
-	process
-	begin
-		print("*****************************************");
-		print("** Testing half_adder...");
-		print("*****************************************");
+  process
+  begin
+    print("*****************************************");
+    print("** Testing half_adder...");
+    print("*****************************************");
 
-		signal_x <= '0'; signal_y <= '0'; wait for PERIOD;
-		adder_result(signal_x, signal_y, signal_cout, signal_sum);
-		signal_x <= '0'; signal_y <= '1'; wait for PERIOD;
-		adder_result(signal_x, signal_y, signal_cout, signal_sum);
-		signal_x <= '1'; signal_y <= '0'; wait for PERIOD;
-		adder_result(signal_x, signal_y, signal_cout, signal_sum);
-		signal_x <= '1'; signal_y <= '1'; wait for PERIOD;
-		adder_result(signal_x, signal_y, signal_cout, signal_sum);
-		wait for PERIOD;
+    signal_x <= '0'; signal_y <= '0'; wait for PERIOD;
+    adder_result(signal_x, signal_y, signal_cout, signal_sum);
+    signal_x <= '0'; signal_y <= '1'; wait for PERIOD;
+    adder_result(signal_x, signal_y, signal_cout, signal_sum);
+    signal_x <= '1'; signal_y <= '0'; wait for PERIOD;
+    adder_result(signal_x, signal_y, signal_cout, signal_sum);
+    signal_x <= '1'; signal_y <= '1'; wait for PERIOD;
+    adder_result(signal_x, signal_y, signal_cout, signal_sum);
+    wait for PERIOD;
 
-		print("*****************************************");
-		print("** FINISHED half_adder test...");
-		print("*****************************************");
-		finish;
-	end process;
+    print("*****************************************");
+    print("** FINISHED half_adder test...");
+    print("*****************************************");
+    finish;
+  end process;
 end architecture behavior;
--- sim/my_package.vhd
+++ sim/my_package.vhd
@@ -5,29 +5,29 @@ use std.env.finish;
 use std.textio.all;
 
 package my_package is
-	procedure print(s: string);
+  procedure print(s: string);
 
-	type t_sample_record is record
-		signal_e: std_logic;
-	end record t_sample_record;
+  type t_sample_record is record
+    signal_e: std_logic;
+  end record t_sample_record;
 
-	function sample_function (
-		input_vector: in std_logic_vector(3 downto 0))
-		return std_logic;
+  function sample_function (
+    input_vector: in std_logic_vector(3 downto 0))
+    return std_logic;
 end package my_package;
 
 package body my_package is
-	procedure print(s: string) is
-		variable l: line;
-	begin
-		write(l, s);
-		writeline(output, l);
-	end procedure print;
+  procedure print(s: string) is
+    variable l: line;
+  begin
+    write(l, s);
+    writeline(output, l);
+  end procedure print;
 
-	function sample_function (
-		input_vector: in std_logic_vector(3 downto 0))
-		return std_logic is
-	begin
-		return '0';
-	end function sample_function;
+  function sample_function (
+    input_vector: in std_logic_vector(3 downto 0))
+    return std_logic is
+  begin
+    return '0';
+  end function sample_function;
 end package body my_package;
