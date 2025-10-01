04c56c5b4ddfd6fb6de229d0b68f09c3911e5127
Francisco <franciscolhernan@gmail.com>
2025-09-30 22:23:51 -0700

    added carry lookahead adder and carry skip adder modules

---
+++ sim/carry_skip_adder_tb.vhd
@@ -0,0 +1,75 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use std.env.finish;
+use std.textio.all;
+
+entity carry_skip_adder_tb is
+end entity carry_skip_adder_tb;
+
+architecture behavior of carry_skip_adder_tb is
+  ---------------------------
+  -- PROCEDURE: print
+  ---------------------------
+  procedure print(s: string) is
+    variable l: line;
+  begin
+    write(l, s);
+    writeline(output, l);
+  end procedure print;
+  ---------------------------
+  -- COMPONENT: carry_skip_adder
+  ---------------------------
+  component carry_skip_adder is
+	port (
+		a: in std_logic_vector(3 downto 0);
+		b: in std_logic_vector(3 downto 0);
+		cin: in std_logic;
+		cout: out std_logic;
+		sum: out std_logic_vector(3 downto 0));
+  end component carry_skip_adder;
+  --------------------------
+  -- SIGNALS
+  --------------------------
+	signal signal_a: std_logic_vector(3 downto 0);
+	signal signal_b: std_logic_vector(3 downto 0);
+	signal signal_cin: std_logic;
+	signal signal_cout: std_logic;
+	signal signal_sum: std_logic_vector(3 downto 0);
+  --------------------------
+  -- CONSTANTS
+  --------------------------
+  constant PERIOD: time := 1 ns;
+begin
+  -----------------------------
+  -- COMPONENT INSTANCE: carry_skip_adder1
+  -----------------------------
+  carry_skip_adder1: component carry_skip_adder
+  port map (
+    a => signal_a,
+    b => signal_b,
+    cin => signal_cin,
+    cout => signal_cout,
+    sum => signal_sum);
+  -----------------------------
+  -- PROCESS: main
+  -- main contains all the signal assignments for 
+  -- the testbench. The results are printed out to
+  -- for text-based confirmation.
+  -----------------------------
+  main: process
+  begin
+    print(LF&"Begin Test...");
+    signal_a <= b"0000"; signal_b <= b"1111"; signal_cin <= '0';  wait for PERIOD;
+    print(to_string(signal_a)&" + "&to_string(signal_b)&" = "&to_string(signal_cout)&" "&to_string(signal_sum));
+    signal_a <= b"0010"; signal_b <= b"1001"; signal_cin <= '0'; wait for PERIOD;
+    print(to_string(signal_a)&" + "&to_string(signal_b)&" = "&to_string(signal_cout)&" "&to_string(signal_sum));
+    signal_a <= b"0110"; signal_b <= b"0101"; signal_cin <= '0'; wait for PERIOD;
+    print(to_string(signal_a)&" + "&to_string(signal_b)&" = "&to_string(signal_cout)&" "&to_string(signal_sum));
+    signal_a <= b"1001"; signal_b <= b"1101"; signal_cin <= '0'; wait for PERIOD;
+    print(to_string(signal_a)&" + "&to_string(signal_b)&" = "&to_string(signal_cout)&" "&to_string(signal_sum));
+    wait for PERIOD;
+    report "Finished test";
+    finish;
+  end process;
+end architecture behavior;
---
+++ src/cla_adder.vhd
@@ -0,0 +1,35 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity cla_adder is
+	port (
+		a: in std_logic_vector(3 downto 0);
+		b: in std_logic_vector(3 downto 0);
+		cin: in std_logic;
+		cout: out std_logic;
+		sum: out std_logic_vector(3 downto 0)
+	);
+end entity cla_adder;
+
+architecture rtl of cla_adder is
+  signal g: std_logic_vector(3 downto 0);
+  signal p: std_logic_vector(3 downto 0);
+  signal c: std_logic_vector(4 downto 0);
+begin
+  p <= a xor b; --propagate
+  g <= a and b; --generate
+
+  --carry generator
+  c(0) <= cin;
+  c(1) <= g(0) or (c(0) and p(0));
+  c(2) <= g(1) or (g(0) and p(1)) or (c(0) and p(0) and p(1));
+  c(3) <= g(2) or (g(1) and p(2)) or (g(0) and p(1) and p(2)) or (c(0) and p(0) and p(1) and p(2));
+  c(4) <= g(3) or (g(2) and p(3)) or (g(1) and p(2) and p(3)) or (g(0) and p(1) and p(2) and p(3)) or (c(0) and p(0) and p(1) and p(2) and p(3));
+  cout <= c(4);
+
+	sum <= p xor c(3 downto 0);
+end rtl;
+
+
+
